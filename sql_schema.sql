ed41f46895363c048f07abe42369e2795ec5b7c1
Jaime M. Shaker <eyeweld@gmail.com>
2022-12-12 19:38:11 -0600

    Adding county data csv file and adding copy statement to schema

--- sql_schema.sql
+++ sql_schema.sql
@@ -22,4 +22,25 @@ DELIMITER ',' CSV HEADER;
 
 SELECT * FROM residential
 
+DROP TABLE IF EXISTS illinois_county_data;
+CREATE TABLE illinois_county_data (
+	county_name TEXT PRIMARY KEY,
+	population_rank SMALLINT,
+	population_2010 int,
+	population_2022 int,
+	population_growth numeric,
+	land_area smallint,
+	population_density numeric,
+	population_percentage numeric
+);
+
+-- Import csv from wheverever you have it stored.  Note the delimiter.
+
+COPY illinois_county_data
+FROM
+'C:\Users\Jaime\Desktop\git-repo\illinois_residential_meth_labs\csv\county_population_2022.csv'
+DELIMITER ',' CSV HEADER;
+
+SELECT * FROM illinois_county_data;
+
 
